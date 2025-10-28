bd3cdbf0f99213bfd942b7c72f64cbf6412be8b6
marii¨ <m.s.nyrop@gmail.com>
2025-10-28 15:46:23 -0400

    document
    Refactor unicode extraction and improve comments.

--- lib/extract_literal_unicode.rb
+++ lib/extract_literal_unicode.rb
@@ -6,16 +6,18 @@ OUTPUT_FILE = 'data/gardiner_hieroglyphs_with_unicode_hex.json'
 
 # read CSV as array of hashes
 glyphs = CSV.parse(File.read(INPUT_FILE), headers: true).map(&:to_h)
+# transform in place, add new `unicode_hex` code directly to hash w/ map!
 glyphs.map! do |glyph|
-  # extract literal unicode from hieroglyph field
+  # extract ASCII literal unicode from hieroglyph field 
+  # use error msg itself to get that info if necessary (char encoding is a weeeeeird thing)
   begin
     unicode = glyph['hieroglyph'].encode("ASCII") # try to encode it to ASCII
   rescue Encoding::UndefinedConversionError # but if that fails
-    unicode= $!.error_char.dump # capture a dump, mapped to the source character
+    unicode = $!.error_char.dump # capture a dump, mapped to the source character
   end
-  glyph['unicode_hex'] = unicode.gsub(/[\D]/, '')
-  glyph
+  glyph['unicode_hex'] = unicode.gsub /[\D]/, '' # store it as clean digits for hex, remove extra non-digit chars
+  glyph # return updated hash
 end
 
-# write to JSON file
-File.write(OUTPUT_FILE, JSON.pretty_generate(glyphs))
+# write results to nicely-formatted JSON file
+File.write OUTPUT_FILE, JSON.pretty_generate(glyphs)
