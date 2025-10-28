52d9a6ee8a01213a6f2703f4a5776337b715af1d
marii¨ <m.s.nyrop@gmail.com>
2025-10-28 15:35:55 -0400

    document

--- lib/generate_pngs_from_unicode.rb
+++ lib/generate_pngs_from_unicode.rb
@@ -1,4 +1,4 @@
-# requires system imagemagick install for `magick` command
+# NOTE: requires system imagemagick install for `magick` command
 
 require 'fileutils'
 require 'json'
@@ -9,16 +9,19 @@ FONT_PATH  = 'lib/font/NotoSansEgyptianHieroglyphs-Regular.ttf'
 
 FileUtils.mkdir_p OUTPUT_DIR
 
+# read JSON as array of (glyph) hashes
 glyphs = JSON.parse(File.read(INPUT_FILE))
 
 glyphs.each do |glyph|
   hieroglyph  = glyph['hieroglyph']
   unicode_hex = glyph['unicode_hex']
+  png_path    = File.join(OUTPUT_DIR, "#{unicode_hex}.png")
   
   next unless !unicode_hex.empty? && !hieroglyph.empty?
 
-  png_path = File.join(OUTPUT_DIR, "#{unicode_hex}.png")
-
   puts "Generating PNG for '#{hieroglyph}' at #{png_path}"
+
+  # direct system call instead of using rgmaick gem (lazy)
+  # takes the UTF glyph directly as label arg, generates & saves image representation
   system `magick -pointsize 400 -font #{FONT_PATH} label:#{hieroglyph} #{png_path}`
 end
